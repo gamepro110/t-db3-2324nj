f35faa77093ac67eff94a7e9e1df0ae6cd70d3da
void@manjaro-legion <k.koelewijn2@gmail.com>
2023-12-22 11:16:50 +0100

    added challenge files

---
+++ SD/c algorithms and data structures/3_Stacks_Queues/mystack/product/main.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <unistd.h>
+
+#include "mystack.h"
+
+int main (int argc, char * argv[])
+{
+    printf("Hello world!\n");
+
+    return 0;
+}
