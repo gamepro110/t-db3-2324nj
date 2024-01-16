dc2770b21d53d7c401ffae80440649af7255693d
void@manjaro-legion <k.koelewijn2@gmail.com>
2024-01-16 22:21:20 +0100

    working on calc logic

--- SD/c algorithms and data structures/3_Stacks_Queues/polish_calc/calc_main.c
+++ SD/c algorithms and data structures/3_Stacks_Queues/polish_calc/calc_main.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <math.h>
 
 typedef struct calc_t Calculation;
 struct calc_t{
@@ -13,18 +14,76 @@ struct calc_t{
 };
 
 void parseNLines(int* nOut);
-void parseSum(void);
+void parseSum(int stackHandle);
 
 int main(void) {
-    printf("%% == √. ^ == ²");
+    printf("%% == √. ^ == ²\n");
     int stackHandle = MystackCreate(sizeof(Calculation));
     int nLines = 0;
     parseNLines(&nLines);
 
-    while (nLines > 0) {
-        parseSum();
+    while (nLines > -1) {
+        parseSum(stackHandle);
         nLines--;
     }
+
+    Calculation calc;
+    while (MystackNofElem(stackHandle) > 0 && MystackPop(stackHandle, &calc) != -1 && calc.numAmount > 0 && calc.numOperands > 0) {
+        int numIdx = 0;
+        int opIdx = 0;
+        int num1 = 0;
+        int num2 = 0;
+
+        while (opIdx < calc.numOperands) {
+            switch (calc.operands[opIdx])
+            {
+            case '+': {
+                num1 = calc.numbers[numIdx];
+                numIdx++;
+                num2 = calc.numbers[numIdx];
+                calc.numbers[numIdx] = (num1 + num2);
+                break;
+            }
+            case '-': {
+                num1 = calc.numbers[numIdx];
+                numIdx++;
+                num2 = calc.numbers[numIdx];
+                calc.numbers[numIdx] = (num1 - num2);
+                break;
+            }
+            case '*': {
+                num1 = calc.numbers[numIdx];
+                numIdx++;
+                num2 = calc.numbers[numIdx];
+                calc.numbers[numIdx] = (num1 *num2);
+                break;
+            }
+            case '/': {
+                num1 = calc.numbers[numIdx];
+                numIdx++;
+                num2 = calc.numbers[numIdx];
+                calc.numbers[numIdx] = (num1 / num2);
+                break;
+            }
+            case '^': {
+                num1 = calc.numbers[numIdx];
+                calc.numbers[numIdx] = (num1 * num1);
+                break;
+            }
+            case '%': {
+                num1 = calc.numbers[numIdx];
+                calc.numbers[numIdx] = sqrt(num1);
+                break;
+            }
+            default:
+                break;
+            }
+            opIdx++;
+        }
+
+        printf("result: %d\n", calc.numbers[numIdx]);
+    }
+    
 
     return MystackDestroy(stackHandle);
 }
@@ -33,13 +92,13 @@ void parseNLines(int* nOut) {
     scanf("%d", nOut);
 }
 
-void parseSum(void) {
+void parseSum(int stackHandle) {
     const uint8_t bufSize = 30;
     char buff[bufSize];
     uint8_t bufIdx = 0;
     char c = 0;
 
-    while (scanf("%c", &c) > 0 && c != '\n' && c != '\000') {
+    while (bufIdx < bufSize && scanf("%c", &c) > 0 && c != '\n' && c != '\000') {
         buff[bufIdx] = c;
         bufIdx++;
         c = 0;
@@ -50,6 +109,8 @@ void parseSum(void) {
     calc->numAmount = 0;
     calc->numOperands = 0;
     int number = 0;
+    int numIdx = 0;
+
     for (int i = 0; i < bufIdx; i++) {
         c = buff[i];
         char num = c - '0';
@@ -57,12 +118,14 @@ void parseSum(void) {
             calc->numbers[calc->numAmount] = number;
             calc->numAmount++;
             number = 0;
+            numIdx = 0;
         }
         else if (num >= 0 && num < 10) { // number found
-            if (i > 0) {
+            if (numIdx > 0) {
                 number *= 10;
             }
             number += c - '0';
+            numIdx++;
         }
         else {
             switch (c)
@@ -81,4 +144,6 @@ void parseSum(void) {
             }
         }
     }
+
+    MystackPush(stackHandle, calc);
 }
